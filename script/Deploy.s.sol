201846f15942fbb58fdad8a65d3058dc0374571f
Zk <zklim95@hotmail.com>
2023-11-19 08:24:43 +0300

    feat: adding qrng and verifier

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,9 +3,11 @@ pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
 import {MatchMaking} from "../src/MatchMaking.sol";
+import {PlayerConfig} from "../src/PlayerConfig.sol";
 
 contract DeployScript is Script {
     MatchMaking mmContract;
+    PlayerConfig pcContract;
 
     function setUp() public {}
 
@@ -13,13 +15,15 @@ contract DeployScript is Script {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        // mmContract = new MatchMaking();
-        mmContract = MatchMaking(0x62125dB2DA783f390CcE30cc354f97c316C62208);
+        mmContract = new MatchMaking();
+        // mmContract = MatchMaking(0x62125dB2DA783f390CcE30cc354f97c316C62208);
         // mmContract.quickMatch{value: 100}();
         // vm.stopBroadcast();
         // vm.startBroadcast(vm.envUint("PRIVATE_KEY2"));
         // mmContract.quickMatch{value: 100}();
-        mmContract.finishMatch(4, 2, 5, 5);
+        // mmContract.finishMatch(4, 2, 5, 5);
+
+        pcContract = new PlayerConfig();
         vm.stopBroadcast();
     }
 }
